c3908e4ffe5ebd6fc87fa032a0078d7132e19d1c
ohirangosta <syuji0116@gmail.com>
2019-02-05 19:10:43 +0900

    modify build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
-as canDoSattack.s -o canDoSattack.o
-gcc canDoSattack.o lib.o -o canDoSattack
+gcc Traditional_DoS/Traditional_DoS.c Traditional_DoS/lib.c -o Traditional_DoS/traditional_DoS
+gcc Random_DoS/Random_DoS.c Random_DoS/lib.c -o Random_DoS/random_DoS
+gcc Targeted_DoS/Targeted_DoS.c Targeted_DoS/lib.c -o Targeted_DoS/targeted_DoS
